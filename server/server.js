addd03641961e5d8a329d4f71f13f16664031fbe
simran <simrangrak1761@gmail.com>
2025-03-13 09:41:35 +0530

    deleted files

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+require('dotenv').config();       //load environment variables
 const express=require('express');
 const cors=require('cors');
 const morgan=require('morgan');
-const http = require("http");
-const { Server } = require("socket.io");
-const connectDB = require("./config/db");
+const http = require("http");   //make an http server
+const { Server } = require("socket.io");    //require server for the real-time connection
+const connectDB = require("./config/db");     //connect with mongodb
 
 
 const app=express();
@@ -15,22 +15,22 @@ connectDB();
 //middleware
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan('dev'));     //for debugging 
 
 const server = http.createServer(app);
-const io = new Server(server, {
+const io = new Server(server, {        //maintains a server and cors for clearing the error between different ports
     cors: {
         origin: "http://localhost:3000",
         methods: ["GET", "POST"],
     },
 });
 
-const webhookRoutes = require("./routes/webhookRoutes")(io);
+const webhookRoutes = require("./routes/webhookRoutes")(io);    //it maintain the different server for the real-time updates
 app.use('/api',webhookRoutes);
 
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
+io.on("connection", (socket) => {        //to check the user connect with real time
+  console.log("A user connected");   
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
@@ -41,6 +41,6 @@ app.get("/", (req, res) => {
   res.send("Server is running...");
 });
 
-server.listen(5000, () => {
+server.listen(PORT, () => {
   console.log("Server is running on port 5000");
 });
